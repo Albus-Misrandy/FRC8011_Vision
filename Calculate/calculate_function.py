Fixed the issue of incorrect camera coordinate calculation

Calculate/calculate_function.py
@@ -13,56 +13,107 @@ def calculate_flat_distance(x, z):
       flat_distance = math.sqrt(x*x + z*z)
       return flat_distance
 
-def check_id(apriltag_id):
+def check_id(apriltag_id, tag_size):
     if apriltag_id == 17:
-        apriltag_world = np.array([[Site_coordinates[0][0]], [Site_coordinates[0][1]], [0.3075]])
+        actual_x = tag_size * math.cos(np.radians(30))
+        actual_y = tag_size * math.sin(np.radians(30))
+        apriltag_world = np.array([
+            [Site_coordinates[0][0] - 0.5 * actual_x, Site_coordinates[0][1] + actual_y * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[0][0] + 0.5 * actual_x, Site_coordinates[0][1] - actual_y * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[0][0] + 0.5 * actual_x, Site_coordinates[0][1] - actual_y * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[0][0] - 0.5 * actual_x, Site_coordinates[0][1] + actual_y * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
         rotate_theta = np.radians(240)
         return apriltag_world, rotate_theta, True
     elif apriltag_id == 18:
-         apriltag_world = np.array([[Site_coordinates[1][0]], [Site_coordinates[1][1]], [0.3075]])
-         rotate_theta = np.radians(180)
-         return apriltag_world, rotate_theta, True
+        apriltag_world = np.array([
+            [Site_coordinates[1][0], Site_coordinates[1][1] + tag_size * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[1][0], Site_coordinates[1][1] - tag_size * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[1][0], Site_coordinates[1][1] - tag_size * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[1][0], Site_coordinates[1][1] + tag_size * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
+        rotate_theta = np.radians(180)
+        return apriltag_world, rotate_theta, True
     elif apriltag_id == 19:
-         apriltag_world = np.array([[Site_coordinates[2][0]], [Site_coordinates[2][1]], [0.3075]])
-         rotate_theta = np.radians(120)
-         return apriltag_world, rotate_theta, True
+        actual_x = tag_size * math.cos(np.radians(30))
+        actual_y = tag_size * math.sin(np.radians(30))
+        apriltag_world = np.array([
+            [Site_coordinates[2][0] + actual_x * 0.5, Site_coordinates[2][1] + actual_y * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[2][0] - actual_x * 0.5, Site_coordinates[2][1] - actual_y * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[2][0] - actual_x * 0.5, Site_coordinates[2][1] - actual_y * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[2][0] + actual_x * 0.5, Site_coordinates[2][1] + actual_y * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
+        rotate_theta = np.radians(120)
+        return apriltag_world, rotate_theta, True
     elif apriltag_id == 20:
-         apriltag_world = np.array([[Site_coordinates[3][0]], [Site_coordinates[3][1]], [0.3075]])
-         rotate_theta = np.radians(60)
-         return apriltag_world, rotate_theta, True
+        actual_x = tag_size * math.cos(np.radians(30))
+        actual_y = tag_size * math.sin(np.radians(30))
+        apriltag_world = np.array([
+            [Site_coordinates[3][0] + 0.5 * actual_x, Site_coordinates[3][1] - actual_y * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[3][0] - 0.5 * actual_x, Site_coordinates[3][1] + actual_y * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[3][0] - 0.5 * actual_x, Site_coordinates[3][1] + actual_y * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[3][0] + 0.5 * actual_x, Site_coordinates[3][1] - actual_y * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
+        rotate_theta = np.radians(60)
+        return apriltag_world, rotate_theta, True
     elif apriltag_id == 21:
-         apriltag_world = np.array([[Site_coordinates[4][0]], [Site_coordinates[4][1]], [0.3075]])
-         rotate_theta = np.radians(0)
-         return apriltag_world, rotate_theta, True
+        apriltag_world = np.array([
+            [Site_coordinates[4][0], Site_coordinates[4][1] - tag_size * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[4][0], Site_coordinates[4][1] + tag_size * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[4][0], Site_coordinates[4][1] + tag_size * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[4][0], Site_coordinates[4][1] - tag_size * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
+        rotate_theta = np.radians(0)
+        return apriltag_world, rotate_theta, True
     elif apriltag_id == 22:
-         apriltag_world = np.array([[Site_coordinates[5][0]], [Site_coordinates[5][1]], [0.3075]])
-         rotate_theta = np.radians(300)
-         return apriltag_world, rotate_theta, True
+        actual_x = tag_size * math.cos(np.radians(30))
+        actual_y = tag_size * math.sin(np.radians(30))
+        apriltag_world = np.array([
+            [Site_coordinates[5][0] - actual_x * 0.5, Site_coordinates[2][1] - actual_y * 0.5, 0.3075 + tag_size * 0.5], # 左上角
+            [Site_coordinates[5][0] + actual_x * 0.5, Site_coordinates[2][1] + actual_y * 0.5, 0.3075 + tag_size * 0.5], # 右上角
+            [Site_coordinates[5][0] + actual_x * 0.5, Site_coordinates[2][1] + actual_y * 0.5, 0.3075 - tag_size * 0.5], # 右下角
+            [Site_coordinates[5][0] - actual_x * 0.5, Site_coordinates[2][1] - actual_y * 0.5, 0.3075 - tag_size * 0.5]  # 左下角
+        ])
+        rotate_theta = np.radians(300)
+        return apriltag_world, rotate_theta, True
     else:
-         apriltag_world = np.array([-1, -1, -1])
+         apriltag_world = np.array([
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1]
+         ])
          rotate_theta = -1
          return apriltag_world, rotate_theta, False
+
+def calculate_camera_points(tag_3d_points, image_points, K_M, D_C):
+    # 使用 PnP 算法估算相机到标签的距离
+    success, rvec, tvec = cv2.solvePnP(tag_3d_points, image_points, K_M, D_C)
+            
+    if success:
+        # 获取旋转矩阵
+        rotation_matrix, _ = cv2.Rodrigues(rvec)
+
+        camera_coordinate = -rotation_matrix.T @ tvec
+
+        return camera_coordinate
     
-def calculate_Rotation_Matrix(theta):
-    Rotation_Matrix = np.array([[math.cos(theta), -math.sin(theta), 0],
-                                [math.sin(theta), math.cos(theta), 0],
-                                [0, 0, 1]])
-    # rvec = np.array([0, 0, theta])
-    # Rotation_Matrix, _ = cv2.Rodrigues(rvec)
-    return Rotation_Matrix
+def calculate_distance_yaw(frame, tag_3d_points, image_points, K_M, D_C):
+     success, rvec, tvec = cv2.solvePnP(tag_3d_points, image_points, K_M, D_C)
 
-def inverse_camera(tevc, R):
-    R_T = np.transpose(R)
-    tevc_inv = -R_T.dot(tevc)
-    return tevc_inv
+     if success:
+        # 计算距离（tvec 是平移向量）
+        distance = np.linalg.norm(tvec)
+        # print(f"Distance to AprilTag: {distance:.2f} meters")
+        # print("rvec:", rvec)
+        # print("tevc:", tvec)
 
-def corrcet_camera_coordinate(tevc):
-    tevc[[1, 2], :] = tevc[[2, 1], :]
-    tevc[0, :] = -tevc[0, :]
-    tevc[1, :] = -tevc[1, :]
-    return tevc
+        tan_yaw = abs(tvec[0][0] / tvec[2][0])
+        # print("tan_yaw:", tvec.shape)
+        yaw = np.arctan(tan_yaw)
+        yaw = np.degrees(yaw)
+        # print("yaw:", yaw)
 
-def calculate_car_position(R_T, cam_in_tag, tag_in_world):
-    cam_coordinate = np.dot(R_T, (cam_in_tag - tag_in_world))
-    return cam_coordinate
-    
+        # 绘制相机位置到标签的向量
+        cv2.putText(frame, f"Distance: {distance:.2f} m", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        return yaw

Camera/apriltag.py
@@ -23,7 +23,7 @@ class AprilTag_Camera():
         
         self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
-    def image_processing(self, frame, detector, K_M, D_C):
+    def image_processing(self, frame, detector):
         # 畸变矫正
         # undistorted_img = cv2.undistort(frame, K_M, D_C)
         # cv2.imshow("Un", undistorted_img)
@@ -38,11 +38,11 @@ class AprilTag_Camera():
 
         return results
     
-    def apriltag_process(self, results, frame, K_M, D_C):
+    def apriltag_process(self, results, frame):
         for r in results:
             # 获取编号
             ID = r.tag_id
-            print(ID)
+            # print(ID)
             # 获取标签的中心位置
             tag_center = (int(r.center[0]), int(r.center[1]))
             # 更新卡尔曼滤波器
@@ -68,28 +68,7 @@ class AprilTag_Camera():
                 [r.corners[3][0], r.corners[3][1]]
             ], dtype=np.float32)
 
-            # 使用 PnP 算法估算相机到标签的距离
-            success, rvec, tvec = cv2.solvePnP(tag_3d_points, image_points, K_M, D_C)
-            
-            if success:
-                # 计算距离（tvec 是平移向量）
-                distance = np.linalg.norm(tvec)
-                print(f"Distance to AprilTag: {distance:.2f} meters")
-
-                # 获取旋转矩阵
-                rotation_matrix, _ = cv2.Rodrigues(rvec)
-
-                # 世界坐标系中的 AprilTag 中心位置
-                tag_position_world = np.dot(rotation_matrix, np.array([0, 0, 0], dtype=np.float32)) + tvec.flatten()
-                print(f"AprilTag position in world coordinates: {tag_position_world}")
-                print("tevc:", tvec)
-                deg = calculate_theta(tvec[0], tvec[2])
-                print("Theta:", deg)
-
-                # 绘制相机位置到标签的向量
-                cv2.putText(frame, f"Distance: {distance:.2f} m", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-                return [ID, distance, tvec, rotation_matrix, deg]
+            return [ID, image_points]
 
     def close(self):
         # 释放摄像头并关闭窗口

main.py
@@ -25,26 +25,20 @@ while True:
 
     # print("Success!")
 
-    results1 = camera_1.image_processing(frame1, detector, K1, dist_coeffs)
-    results2 = camera_2.image_processing(frame2, detector, K2, dist_coeffs)
+    results1 = camera_1.image_processing(frame1, detector)
+    results2 = camera_2.image_processing(frame2, detector)
 
-    info1 = camera_1.apriltag_process(results1, frame1, K1, dist_coeffs)
-    info2 = camera_2.apriltag_process(results2, frame2, K2, dist_coeffs)
+    info1 = camera_1.apriltag_process(results1, frame1)
+    info2 = camera_2.apriltag_process(results2, frame2)
 
     if info1:
         print("Fuck yes")
-        apriltag_world, rotate_theta, is_apriltag = check_id(info1[0])
-        if is_apriltag:
-            R_w2c = calculate_Rotation_Matrix(rotate_theta)
-            R_c2w = np.transpose(R_w2c)
-            # cam_in_tag_coordinate = inverse_camera(info1[2], info1[3])
-            cam_in_tag_coordinate = corrcet_camera_coordinate(info1[2])
-            cam_position = calculate_car_position(R_c2w, cam_in_tag_coordinate, apriltag_world)
-            print("R:", R_w2c)
-            print("Camera_coordinate:", cam_position)
-            print("cam_in_tag:", cam_in_tag_coordinate)
-            print("apriltag_world:", apriltag_world)
-        # calculate_car_position(is_apriltag, apriltag_world, info1[2], info1[3], info1[4])
+        # print("image_Points:", info1[1])
+        apriltag_world, rotate_theta, is_apriltag = check_id(info1[0], 0.165)
+        yaw1 = calculate_distance_yaw(frame1, tag_3d_points, info1[1], K1, dist_coeffs)
+        cam1_points = calculate_camera_points(apriltag_world, info1[1], K1, dist_coeffs)
+        # print("Points:", cam1_points)
+        Net.send_double_data("yaw", yaw1)
     elif info2 and info1 == None:
         print("Fuck")
     else: